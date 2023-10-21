47459e0a8fc70dc1d262b570a6a3ad8b9b2563c7
black-desk <me@black-desk.cn>
2023-10-21 23:09:05 +0800

    chore: add export
    Signed-off-by: black-desk <me@black-desk.cn>

--- get.sh
+++ get.sh
@@ -13,12 +13,14 @@ test -z "$USERNAME" && {
 	echo "User name missing." >&2
 	exit 1
 }
+export USERNAME
 
 test -z "$REPO" && REPO="$2"
 test -z "$REPO" && {
 	echo "Repository missing." >&2
 	exit 1
 }
+export REPO
 
 # https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
 get_latest_release() {
@@ -37,18 +39,27 @@ test -z "$VERSION" && {
 	echo "Unable to get $USERNAME/$REPO version." >&2
 	exit 1
 }
+export VERSION
 
 test -z "$BASENAME" && BASENAME=$REPO
+export BASENAME
 
 TMP_DIR="$(mktemp -d)"
+export TMP_DIR
+
 # shellcheck disable=SC2064 # intentionally expands here
 trap "rm -rf \"$TMP_DIR\"" EXIT INT TERM
 
 OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
+export OS
+
 ARCH="$(uname -m)"
+export ARCH
 test "$ARCH" = "aarch64" && ARCH="arm64"
 test "$ARCH" = "x86_64" && ARCH="amd64"
+
 TAR_FILE="${BASENAME}_${VERSION}_${OS}_${ARCH}.tar.gz"
+export TAR_FILE
 
 cd "$TMP_DIR"
 echo "Downloading $BASENAME $VERSION..."
@@ -56,10 +67,11 @@ curl -fLO "https://github.com/$USERNAME/$REPO/releases/download/v$VERSION/$TAR_F
 tar -xf "$TMP_DIR/$TAR_FILE" -C "$TMP_DIR"
 
 SUDO=sudo
+export SUDO
 
 if command pkexec >/dev/null 2>&1; then
-	# shellcheck disable=SC2034
 	SUDO=pkexec
 fi
 
 test -z "$PREFIX" && PREFIX="/usr/local"
+export PREFIX
